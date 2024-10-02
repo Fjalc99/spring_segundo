d631a4ae3680ab5f24de213f89c845a654fef49a
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-10-02 18:40:53 +0200

    Monumentos

--- demo1/src/main/java/com/salesianostriana/dam/monumentos/controller/MonumentosController.java
+++ demo1/src/main/java/com/salesianostriana/dam/monumentos/controller/MonumentosController.java
@@ -2,7 +2,9 @@ package com.salesianostriana.dam.monumentos.controller;
 
 import com.salesianostriana.dam.monumentos.model.Monumento;
 import com.salesianostriana.dam.monumentos.repository.MonumentosRepository;
+import com.salesianostriana.dam.monumentos.service.MonumentosService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,57 +17,54 @@ public class MonumentosController {
     private final MonumentosRepository monumentosrepository;
 
 
+    private final MonumentosService monumentosservice;
+    private final MonumentosService monumentosService;
+
 
     @GetMapping("/monumento/")
-    public ResponseEntity<List<Monumento>> getAll() {
-        List<Monumento> result = monumentosrepository.findAll();
+    public ResponseEntity<List<Monumento>> obtenerMonumentos() {
+        List<Monumento> monumentos = monumentosrepository.findAll();
 
-        if (result.isEmpty())
+        if (monumentos.isEmpty())
             return ResponseEntity.notFound().build();
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(monumentos);
 
     }
 
 
     @GetMapping("/monumento/{id}")
-    public ResponseEntity<Monumento> getById(@PathVariable Long id) {
-        return ResponseEntity.of(monumentosrepository.findById(id));
+    public ResponseEntity<Monumento> obtenerMonumentoPorId(@PathVariable Long id) {
+        Monumento monumento = monumentosrepository.findById(id).orElse(null);
+
+        return monumento != null ? ResponseEntity.ok(monumento) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
     }
 
     @PostMapping("/monumento/")
-    public ResponseEntity<Monumento> create(@RequestBody Monumento monumento) {
-        Monumento nuevo = monumentosrepository.save(monumento);
+    public ResponseEntity<Monumento> guardarMonumento(@RequestBody Monumento monumento) {
+        Monumento nuevoMonumento = monumentosrepository.save(monumento);
 
-        return ResponseEntity.status(201).body(nuevo);
+        return ResponseEntity.status(201).body(nuevoMonumento);
     }
 
     @PutMapping("/monumento/{id}")
-    public ResponseEntity<Monumento> edit(@PathVariable Long id,
-                                       @RequestBody Monumento monumento) {
+    public ResponseEntity<Monumento> editarMonumento(@PathVariable Long id, @RequestBody Monumento monumento) {
 
-        return ResponseEntity.of(monumentosrepository.findById(id)
-                .map(antiguo -> {
-                        antiguo.setNombrePais(monumento.getNombrePais());
-                        antiguo.setImagen(monumento.getImagen());
-                        antiguo.setLatitud(monumento.getLatitud());
-                        antiguo.setLongitud(monumento.getLongitud());
-                        antiguo.setCodigoPais(monumento.getCodigoPais());
-                        antiguo.setNombreCiudad(monumento.getNombreCiudad());
-                        antiguo.setNombreMonumento(monumento.getNombreMonumento());
-                        antiguo.setDescripcion(monumento.getDescripcion());
-                    return monumentosrepository.save(antiguo);
-                }));
+        return ResponseEntity.ok(monumentosService.editarMonumento(monumento,id));
     }
 
     @DeleteMapping("/monumento/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<?> eliminarMonumento(@PathVariable Long id) {
 
-        if (monumentosrepository.existsById(id))
-            monumentosrepository.deleteById(id);
+        if (monumentosrepository.existsById(id)){
+            monumentosService.eliminarMonumento(id);
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
 
-        return ResponseEntity.noContent().build();
+
 
     }
 
